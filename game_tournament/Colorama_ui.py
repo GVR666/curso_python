76ec37cf278178e36d0448090ce04778c2332159
GVR666 <guillermovazquezrojas029@gmail.com>
2026-03-03 18:54:10 -0700

    ACTUALIZADO

--- game_tournament/Colorama_ui.py
+++ game_tournament/Colorama_ui.py
@@ -4,7 +4,7 @@ from Tournament import Tournament
 from colorama import  Fore, Back, Style, init
 init(autoreset=True)
 
-class coloramaUI:
+class ColoramaUI:
     def __inti__ (self):
         self.tournament = None
         self.current_file = None
@@ -56,8 +56,36 @@ class coloramaUI:
         def exit_app(self):
             print("Exiting application...")
             exit()
-if __name__ == "__Main__":
-    ui = ColoramaUI()
-    ui.run()
+        def get_tournament_json(self):
+            file_path = input("Enter the path to the json file:")
+            self.set_current_file(file_path)
+            self.open_tournament(file_path)
+        def display_menu(self):
+            dictionary_menu = {
+                "1": "Load tournament",
+                "2": "Display tournament",
+                "3": "Exit",
+            }
+        action_dictionary = {
+            "1": self.get_tournament_json,
+            "2": self.display_tournament,
+            "3": self.exit_app
+        }
+        while true:
+           print("\nTournament")
+           for key in sorted (dictionary_menu.keys()):
+               print(f"(key),{dictionary_menu[key]}")
+           choice = input("nter your choice:")
+           if choice in action_dictionary:
+               action_dictionary[choice]()
+           else:
+               print("invalid choice. Please try again.")
 
         
+if __name__ == "__main__":
+    print ("Corri")
+    ui = ColoramaUI()
+    ui.set_current_file("Tournament.json")
+    ui.open_tournament()
+    ui.run()
+
