a97fca147d813400687f95dc7ba762222524b2aa
GVR666 <guillermovazquezrojas029@gmail.com>
2026-03-23 18:24:36 -0700

    Books añadido

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/') # Home or root of site
 def index():
@@ -10,5 +10,5 @@ def index():
 def about():
     return '<html><head><title>HELLO WORLD</title>About this page</title></head><body> Everything about this website. Back to <a href="/">Hello world</a></body></html>'
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True)
