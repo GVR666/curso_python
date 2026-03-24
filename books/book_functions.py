8fa69df8c4afe676e4c26c9284f8d288f6b2bef7
GVR666 <guillermovazquezrojas029@gmail.com>
2026-03-23 18:50:54 -0700

    book_functions.py agregado(no terminado)

---
+++ books/book_functions.py
@@ -0,0 +1,16 @@
+from Book import Book
+from Book import load_books
+
+def get_genres(books_list[Book]->List(str))
+    genres = set()
+    for book in books:
+        genres.add(book.genre)
+    return sorted (list(genres))
+def create_author_dictionary(books:list[Book)->dict[str, List(Book)]
+    autor_dict = {}
+    for book in books:
+        
+       if book.author.lower( not in author_dict:
+            
+if __name__ == "__main__":
+    
